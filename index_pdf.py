c03eab9b4c82bcb566065e15eeba1f6fb61c7fb9
kimin <thekiminlee@gmail.com>
2022-04-05 22:15:44 -0600

    scalable fix

--- index_pdf.py
+++ index_pdf.py
@@ -1,20 +1,27 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import sys
+import os
+
+path = "/Users/kimin/Desktop/"
+start_page = int(sys.argv[3])
+end_page = int(sys.argv[4])
 
 
-def indexPDF(argv):
-    filename, export_path = argv[1], argv[2]
-    start_page, end_page = int(argv[3]), int(argv[4])
+def indexPDF():
+    filename, export_file =sys.argv[1], sys.argv[2]
+    export_path = os.path.join(path, export_file)
 
     reader = PdfFileReader(open(filename, 'rb'))
     printer = PdfFileWriter()
 
-    for page in range(start_page-1, end_page):
+    for page in range(start_page-1, end_page-1):
+        print(page)
         printer.addPage(reader.getPage(page))
+
 
     with open(export_path, 'wb') as pdf:
         printer.write(pdf)
 
 
 if __name__ == '__main__':
-    indexPDF(sys.argv)
+    indexPDF()
